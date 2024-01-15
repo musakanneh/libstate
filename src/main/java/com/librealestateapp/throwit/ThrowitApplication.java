f5cf1f5b948351b977daab186529e5e75bc78538
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2024-01-15 21:10:09 +0000

    first commit

---
+++ src/main/java/com/librealestateapp/throwit/ThrowitApplication.java
@@ -0,0 +1,12 @@
+package com.librealestateapp.throwit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrowitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/librealestateapp/throwit/ThrowitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librealestateapp.throwit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrowitApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
